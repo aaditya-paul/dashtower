145ef07c86d7a4b1fb0af2cdbd5c2d9c87bff816
Subha Chanda <subhachanda88@gmail.com>
2022-08-04 23:16:07 +0530

    converted index.js to next

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import Aos from 'aos';
+
+import '../styles/globals.css';
+
+import 'aos/dist/aos.css';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const location = router.pathname;
+
+  useEffect(() => {
+    Aos.init({
+      once: true,
+      disable: 'phone',
+      duration: 600,
+      easing: 'ease-out-sine',
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto';
+    window.scroll({ top: 0 });
+    document.querySelector('html').style.scrollBehavior = '';
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <div className='font-inter antialiased bg-gray-900 text-gray-200 tracking-tight'>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
