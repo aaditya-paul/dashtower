891c7a64e25addf3568176bbfa22892f5818910c
nemo0 <subhachanda88@gmai.com>
2022-08-06 00:09:33 +0530

    added modal

--- components/partials/HeroHome.jsx
+++ components/partials/HeroHome.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import Modal from '../utils/Modal';
+
+import { Dialog, Transition } from '@headlessui/react';
+
+import ContactModal from '../utils/ContactModal';
 
 function HeroHome() {
   const [videoModalOpen, setVideoModalOpen] = useState(false);
+  let [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
 
   return (
     <section>
@@ -65,10 +78,65 @@ function HeroHome() {
                 <a
                   className='btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0'
                   href='#0'
+                  onClick={openModal}
                 >
                   Supercharge
                 </a>
               </div>
+
+              {/* Modal */}
+              <Transition appear show={isOpen} as={Fragment}>
+                <Dialog as='div' className='relative z-10' onClose={closeModal}>
+                  <Transition.Child
+                    as={Fragment}
+                    enter='ease-out duration-300'
+                    enterFrom='opacity-0'
+                    enterTo='opacity-100'
+                    leave='ease-in duration-200'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                  >
+                    <div className='fixed inset-0 bg-black bg-opacity-25' />
+                  </Transition.Child>
+
+                  <div className='fixed inset-0 overflow-y-auto'>
+                    <div className='flex min-h-full items-center justify-center p-4 text-center'>
+                      <Transition.Child
+                        as={Fragment}
+                        enter='ease-out duration-300'
+                        enterFrom='opacity-0 scale-95'
+                        enterTo='opacity-100 scale-100'
+                        leave='ease-in duration-200'
+                        leaveFrom='opacity-100 scale-100'
+                        leaveTo='opacity-0 scale-95'
+                      >
+                        <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                          <Dialog.Title
+                            as='h3'
+                            className='text-lg font-medium leading-6 text-gray-900'
+                          >
+                            Contact Us To Get Your Free Audit Report
+                          </Dialog.Title>
+                          <div className='mt-2'>
+                            <ContactModal />
+                          </div>
+
+                          <div className='mt-4'>
+                            <button
+                              type='button'
+                              className='w-full justify-center rounded-sm border border-transparent px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
+                              onClick={closeModal}
+                            >
+                              I don't need free audit!
+                            </button>
+                          </div>
+                        </Dialog.Panel>
+                      </Transition.Child>
+                    </div>
+                  </div>
+                </Dialog>
+              </Transition>
+
               <div data-aos='fade-up' data-aos-delay='600'>
                 <a
                   className='btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4'
