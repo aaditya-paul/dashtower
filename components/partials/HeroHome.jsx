56b69e98108ea0e970cab6281bedff128f52f7cd
nemo0 <subhachanda88@gmai.com>
2022-08-05 23:53:13 +0530

    updated logo

--- components/partials/HeroHome.jsx
+++ components/partials/HeroHome.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Modal from '../utils/Modal';
+import { Dialog } from '@headlessui/react';
 
 function HeroHome() {
   const [videoModalOpen, setVideoModalOpen] = useState(false);
